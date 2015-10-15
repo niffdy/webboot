401803a67bd5c17d2c2299be749acd0f9c7f413c
Brad Sjue <bsjue@cerner.com>
2015-10-15 00:00:41 -0500

    foreach loop in downloaded text

--- webboot.ps1
+++ webboot.ps1
@@ -1,3 +1,13 @@
 ﻿$url = "https://raw.githubusercontent.com/niffdy/webboot/master/lading.txt"
 $ladingtxt = "$env:TEMP\lading.txt"
 (New-Object System.Net.WebClient).DownloadFile($url, $ladingtxt)
+
+ForEach ($line in (Get-Content $ladingtxt)) {
+  # $line -replace "\d","x" | Out-File new.txt -Append
+   $basefilename = Split-Path $line -Leaf
+   $newcmdfile = "$env:TEMP\$basefilename"
+   $newcmdfile
+   Write-Host $line
+   (New-Object System.Net.WebClient).DownloadFile($line, $newcmdfile)
+   
+}
